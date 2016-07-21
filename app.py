8272896ce8d88fb059992dffdcadc0c8800f3bbe
Elie Schoppik <eschoppik@gmail.com>
2016-07-20 21:34:33 -0700

    deploy

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from project import app
+import os
 
+port = os.environ.get('PORT')
+debug = os.environ.get('DEBUG')
 
 if __name__ == '__main__':
 	print('running on port 3000')
-	app.run(debug=True, port=3000)
+	app.run(debug=debug, port=port)
 
 
